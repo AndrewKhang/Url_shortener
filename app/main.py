8c72aaa7af24650601b6dea296aeee91edd36f1a
AndrewKhang <136875543+AndrewKhang@users.noreply.github.com>
2026-05-14 17:01:45 +0700

    Frontend Connection

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ from app.database import engine, Base
 from app.models import link
 from app.routes import link
 from app.models import click
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(link.router)
 
