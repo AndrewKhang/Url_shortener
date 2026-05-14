6d4d480388f721b510913742e1c89f50ff1bed2a
AndrewKhang <136875543+AndrewKhang@users.noreply.github.com>
2026-05-14 13:55:41 +0700

    improve robustness and tracking

--- app/routes/link.py
+++ app/routes/link.py
@@ -1,4 +1,3 @@
-from urllib import request
 from app.services.shortener import generate_short_key,get_location
 from app.schemas.link import LinkResponse, LinkCreate
 from fastapi import APIRouter, Depends, HTTPException
@@ -11,6 +10,10 @@ from datetime import datetime
 from app import cache
 from fastapi import Request
 from user_agents import parse
+from datetime import datetime, timedelta
+from fastapi import BackgroundTasks
+
+
 router = APIRouter(prefix="", tags=["links"])
 
 @router.post("/links",response_model=LinkResponse)
@@ -21,40 +24,62 @@ def receive_link(data: LinkCreate, request : Request,db: Session = Depends(get_d
     # Redis key for this IP
     rate_key = f"rate:{ip}"
     
-    # Check how many requests this IP made
-    count = cache.connection.get(rate_key)
-    if count and int(count) >= 10:
+# Atomic rate limit check using Lua script
+    lua_script = """
+    local count = redis.call('INCR', KEYS[1])
+    if count == 1 then
+    redis.call('EXPIRE', KEYS[1], 3600)
+    end
+    return count
+    """
+    count = cache.connection.eval(lua_script, 1, rate_key)
+    if count > 10:
         raise HTTPException(status_code=429, detail="Too many requests")
-    pipe = cache.connection.pipeline()
-    pipe.incr(rate_key)
-    pipe.expire(rate_key, 3600)  # 1 hour
-    pipe.execute()
-    short_key = generate_short_key(db)
+    # Check how many requests this IP made
+    if data.custom_alias:
+        existing = db.query(Link).filter(Link.short_key == data.custom_alias).first()
+        if existing:
+             raise HTTPException(status_code=409, detail="Alias already exists")
+        short_key = data.custom_alias
+    else:
+        short_key = generate_short_key(db)
+    if not data.expire_date:
+        expire_date = datetime.utcnow() + timedelta(days=30)
+    else:
+        expire_date = data.expire_date
     link = Link(
-    original_url=data.original_url,
-    short_key=short_key,
-    custom_alias=data.custom_alias,
-    expire_date=data.expire_date
-)
+        original_url=str(data.original_url),
+        short_key=short_key,
+        custom_alias=data.custom_alias,
+        expire_date=expire_date
+    )
     db.add(link)
-    db.commit()                                   # only commit after email sent successfully
+    db.commit()                               # only commit after email sent successfully
     db.refresh(link)
     return link
 
+def sync_click_count(short_key:str,db: Session):
+    count = cache.connection.get(f"clicks:{short_key}")
+    if count:
+        db.query(Link).filter(Link.short_key == short_key).update(
+                {"click_count": int(count)}
+        )
+        db.commit()
 @router.get("/{short_key}")
-def get_link( request:Request, short_key: str, db: Session = Depends(get_db)):
+def get_link(request: Request, short_key: str, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     ua = parse(request.headers.get("user-agent", ""))
     device = f"{ua.browser.family} on {ua.os.family}"
     click = Click(
     short_key=short_key,
     device=device,
-    location=get_location(request.client.host)  # tạm thời, sau thêm geolocation
+    location=get_location(request.client.host)  
 )
     cache_url= cache.connection.get(short_key)
     if cache_url:
         cache.connection.incr(f"clicks:{short_key}")
         db.add(click)
         db.commit()
+        background_tasks.add_task(sync_click_count,short_key,db)
         return RedirectResponse(cache_url, status_code=301)
     link = db.query(Link).filter(Link.short_key == short_key).first()
     if not link:
@@ -67,5 +92,6 @@ def get_link( request:Request, short_key: str, db: Session = Depends(get_db)):
      cache.connection.incr(f"clicks:{short_key}")
      db.add(click)
      db.commit()
+     background_tasks.add_task(sync_click_count,short_key,db)
      return RedirectResponse(link.original_url, status_code=301)
 
--- app/schemas/link.py
+++ app/schemas/link.py
@@ -1,17 +1,17 @@
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
-
+from pydantic import BaseModel, AnyHttpUrl
 # Schema for incoming request — what user sends
 class LinkCreate(BaseModel):
-    original_url: str
+    original_url: AnyHttpUrl
     custom_alias: Optional[str] = None  # user can customize short key
     expire_date: Optional[datetime] = None  # optional expiry
 
 # Schema for response — what we return to user
 class LinkResponse(BaseModel):
     short_key: str
-    original_url: str
+    original_url: AnyHttpUrl
     expire_date: Optional[datetime] = None
     created_at: datetime
 
